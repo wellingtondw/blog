b29aaecbffbacb0a63a1ffee1898752f03715ca1
wellington <wellingtonslpbi@hotmail.com>
2021-03-27 10:35:49 -0300

    create header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,13 @@
+import Link from 'next/link'
+
+import styles from './header.module.scss'
+
 export default function Header() {
-  // TODO
+  return (
+    <header className={styles.header} >
+      <Link href='/'>
+        <img src='/images/logo.svg' alt='logo'/>
+      </Link>
+    </header>
+  )
 }
